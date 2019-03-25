fb2b51544bc40f901a6f7b5d6aa2fda89a7c6a85
Jeremy Jackson <jeremy@bluewall.com>
2019-03-25 16:16:49 -0500

    Fix quote

--- src/commands/create.ts
+++ src/commands/create.ts
@@ -60,7 +60,7 @@ export default class Create extends Command {
    * @var  {string[]}
    */
   static examples = [
-    `$ combuilder create NAME VIEW`,
+    '$ combuilder create NAME VIEW',
   ];
 
   static flags = {
